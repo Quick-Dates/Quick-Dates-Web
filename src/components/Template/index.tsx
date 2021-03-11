6b720c70646e1c72f31d1180432205a3738ed424
WillianFernandes17 <william.fernandes@totemti.com.br>
2021-03-11 17:35:22 -0400

    ajuste no footer

--- src/components/Template/index.tsx
+++ src/components/Template/index.tsx
@@ -1,6 +1,7 @@
 import React, { FC, ReactNode } from 'react';
 import Header from '../Header';
 import Footer from '../Footer';
+import './styles.css';
 
 interface IProps{
   // eslint-disable-next-line react/require-default-props
