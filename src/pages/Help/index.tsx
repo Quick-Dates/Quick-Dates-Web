4bcfd523fa271bf1cac75cf59ee2fa8fbd61fc37
Vitor-Manoel1014 <vm53596@gmail.com>
2021-03-11 17:29:19 -0400

    Criação da página Help

---
+++ src/pages/Help/index.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+import Template from '../../components/Template';
+import './styles.css';
+
+const Help: FC = () => (
+  <div>
+    <Template isStudent>
+      <h1 className="title">Ajuda</h1>
+    </Template>
+  </div>
+);
+
+export default Help;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,14 @@
 import React, { FC } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import ActivitiesList from '../pages/ActivitiesList';
+import Help from '../pages/Help';
 import Login from '../pages/Login';
 
 const Routes: FC = () => (
   <Switch>
     <Route path="/" exact component={Login} />
     <Route path="/activities" component={ActivitiesList} />
+    <Route path="/help" component={Help} />
   </Switch>
 );
 
