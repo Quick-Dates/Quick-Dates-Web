29d71a57d46ab73264f953b5ca50cc1af67b8900
Gustavo Lários <gusttalarios14@gmail.com>
2021-03-12 08:12:32 -0400

    Adding titles

--- src/pages/Help/index.tsx
+++ src/pages/Help/index.tsx
@@ -1,9 +1,14 @@
 import React, { FC } from 'react';
 import Template from '../../components/Template';
 import './styles.css';
+// eslint-disable-next-line import/order
+import Helmet from 'react-helmet';
 
 const Help: FC = () => (
   <div>
+    <Helmet>
+      <title>Quick Dates | Help</title>
+    </Helmet>
     <Template isStudent>
       <h1 className="title">Ajuda</h1>
     </Template>
