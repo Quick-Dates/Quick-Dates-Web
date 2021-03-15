a458ca83e0631d3ea6bbb94a0b91be500f47946c
Gustavo Lários <gusttalarios14@gmail.com>
2021-03-15 18:17:45 -0400

    Calendar styles

--- src/pages/ActivitiesList/index.tsx
+++ src/pages/ActivitiesList/index.tsx
@@ -1,15 +1,42 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import BigCalendar from '../../components/BigCalendar/index';
 import Template from '../../components/Template';
 import './styles.css';
 
+// animations
+// eslint-disable-next-line import/order
+import Lottie from 'react-lottie';
+import animation from '../../assets/calendar-animation.json';
+
 export default function ActivitiesList() {
+  const [isAnimation, setAnimation] = useState(true);
+
+  setTimeout(() => { setAnimation(false); }, 8000);
+
+  const optionsAnimation = {
+    loop: true,
+    autoplay: true,
+    animationData: animation,
+  };
+
   return (
-    <div>
-      <Template isStudent title="Calendar">
-        <h1 className="title">Atividades</h1>
-        <BigCalendar />
-      </Template>
-    </div>
+
+    <>
+      {isAnimation ? (
+        <div className="content">
+          <Lottie
+            options={optionsAnimation}
+            height={600}
+            width={600}
+          />
+        </div>
+      ) : (
+        <Template isStudent title="Calendar">
+          <div className="content">
+            <BigCalendar />
+          </div>
+        </Template>
+      ) }
+    </>
   );
 }
