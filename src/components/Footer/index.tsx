95871c282110d5f228cdc85a1049ddb5e0a43ad9
WillianFernandes17 <william.fernandes@totemti.com.br>
2021-03-15 13:25:17 -0400

    link instagram

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-target-blank */
 import React, { FC } from 'react';
 import InstagramIcon from '../../assets/instagram.svg';
 import './styles.css';
@@ -5,7 +6,7 @@ import './styles.css';
 const Footer: FC = () => (
   <footer className="footer">
     <p className="text">© Quick Dates 2020</p>
-    <a href="https://www.instagram.com/">
+    <a href="https://www.instagram.com/quick.dates/" target="_blank">
       <img src={InstagramIcon} alt="instagram" />
     </a>
   </footer>
