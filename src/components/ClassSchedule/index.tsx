859528509d4630e3ad4b16a25500c782e76e983c
Vitor-Manoel1014 <vm53596@gmail.com>
2021-03-16 16:55:22 -0400

    Termino da página Time

--- src/components/ClassSchedule/index.tsx
+++ src/components/ClassSchedule/index.tsx
@@ -2,8 +2,100 @@ import React, { FC } from 'react';
 import './styles.css';
 
 const ClassSchedule: FC = () => (
-  <div className="classSchedule">
-    <h1>Horário</h1>
+  <div className="container">
+    <div className="horarios">
+      <span className="horario">07:00 - 07:50</span>
+      <span className="horario">07:50 - 08:40</span>
+      <span className="horario">08:40 - 09:45</span>
+      <span className="horario">07:00 - 07:50</span>
+      <span className="horario">07:00 - 07:50</span>
+      <span className="horario">07:00 - 07:50</span>
+      <span className="horario">07:00 - 07:50</span>
+      <span className="horario">07:00 - 07:50</span>
+      <span className="horario">07:00 - 07:50</span>
+      <span className="horario">07:00 - 07:50</span>
+    </div>
+    <div className="table">
+      <div className="headerDias">
+        <span className="dia">Segunda</span>
+        <span className="dia">Terça</span>
+        <span className="dia">Quarta</span>
+        <span className="dia">Quinta</span>
+        <span className="dia">Sexta</span>
+      </div>
+      <div className="content">
+        <div className="line">
+          <span className="materia">Português</span>
+          <span className="materia">Português</span>
+          <span className="materia">Matematica</span>
+          <span className="materia">Matematica</span>
+          <span className="materia">Filosofia</span>
+        </div>
+        <div className="line">
+          <span className="materia">Portugues</span>
+          <span className="materia">Portugues</span>
+          <span className="materia">Matematica</span>
+          <span className="materia">Matematica</span>
+          <span className="materia">Filosofia</span>
+        </div>
+        <div className="line">
+          <span className="materia">Portugues</span>
+          <span className="materia">Portugues</span>
+          <span className="materia">Matematica</span>
+          <span className="materia">Matematica</span>
+          <span className="materia">Filosofia</span>
+        </div>
+        <div className="line">
+          <span className="materia">Portugues</span>
+          <span className="materia">Portugues</span>
+          <span className="materia">Matematica</span>
+          <span className="materia">Matematica</span>
+          <span className="materia">Filosofia</span>
+        </div>
+        <div className="line">
+          <span className="materia">Portugues</span>
+          <span className="materia">Portugues</span>
+          <span className="materia">Matematica</span>
+          <span className="materia">Matematica</span>
+          <span className="materia">Filosofia</span>
+        </div>
+        <div className="line">
+          <span className="materia">Portugues</span>
+          <span className="materia">Portugues</span>
+          <span className="materia">Matematica</span>
+          <span className="materia">Matematica</span>
+          <span className="materia">Filosofia</span>
+        </div>
+        <div className="line">
+          <span className="materia">Portugues</span>
+          <span className="materia">Portugues</span>
+          <span className="materia">Matematica</span>
+          <span className="materia">Matematica</span>
+          <span className="materia">Filosofia</span>
+        </div>
+        <div className="line">
+          <span className="materia">Portugues</span>
+          <span className="materia">Portugues</span>
+          <span className="materia">Matematica</span>
+          <span className="materia">Matematica</span>
+          <span className="materia">Filosofia</span>
+        </div>
+        <div className="line">
+          <span className="materia">Portugues</span>
+          <span className="materia">Portugues</span>
+          <span className="materia">Matematica</span>
+          <span className="materia">Matematica</span>
+          <span className="materia">Filosofia</span>
+        </div>
+        <div className="line">
+          <span className="materia">Portugues</span>
+          <span className="materia">Portugues</span>
+          <span className="materia">Matematica</span>
+          <span className="materia">Matematica</span>
+          <span className="materia">Filosofia</span>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
