d6ba23b9caba454d6db3f9d8799fe0b5e3d27f05
Gustavo Lários <gusttalarios14@gmail.com>
2021-03-19 18:04:43 -0400

    erros clear

--- src/components/BigCalendar/index.tsx
+++ src/components/BigCalendar/index.tsx
@@ -1,5 +1,5 @@
 import moment from 'moment';
-import React, { useState } from 'react';
+import React from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import 'moment/locale/pt-br';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
