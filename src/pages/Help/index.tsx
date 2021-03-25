12c5a901bf03341de47355c4443680c8e76e24b6
Willian17 <willianfernandes113@gmail.com>
2021-03-25 15:39:39 -0400

    resolvendo erro eslint

--- src/pages/Help/index.tsx
+++ src/pages/Help/index.tsx
@@ -14,23 +14,21 @@ interface IProps{
 export default function Help() {
   const { user }:IProps = useParams();
   return (
-    <div>
-      <Template isStudent={user === 'student' ? true : false} title="Help">
-        <div className="socialMedia">
-          <a className="linksSocialMedia" href="https://web.telegram.org/#/login">
-            <img className="socialMediaIcon" src={TelegramIcon} alt="Telegram" />
-            <p className="nameSocialMedia">Telegram</p>
-          </a>
-          <a className="linksSocialMedia" href="https://www.gmail.com/mail/help/intl/en/about.html?utm_expid=...">
-            <img className="socialMediaIcon" src={GmailIcon} alt="Gmail" />
-            <p className="nameSocialMedia">Gmail</p>
-          </a>
-          <a className="linksSocialMedia" href="https://www.whatsapp.com/?lang=pt_br">
-            <img className="socialMediaIcon" src={WhatsAppIcon} alt="WhatsApp" />
-            <p className="nameSocialMedia"> WhatsApp </p>
-          </a>
-        </div>
-      </Template>
-    </div>
+    <Template isStudent={user === 'student' ? true : false} title="Help">
+      <div className="socialMedia">
+        <a className="linksSocialMedia" href="https://web.telegram.org/#/login">
+          <img className="socialMediaIcon" src={TelegramIcon} alt="Telegram" />
+          <p className="nameSocialMedia">Telegram</p>
+        </a>
+        <a className="linksSocialMedia" href="https://www.gmail.com/mail/help/intl/en/about.html?utm_expid=...">
+          <img className="socialMediaIcon" src={GmailIcon} alt="Gmail" />
+          <p className="nameSocialMedia">Gmail</p>
+        </a>
+        <a className="linksSocialMedia" href="https://www.whatsapp.com/?lang=pt_br">
+          <img className="socialMediaIcon" src={WhatsAppIcon} alt="WhatsApp" />
+          <p className="nameSocialMedia"> WhatsApp </p>
+        </a>
+      </div>
+    </Template>
   );
 }
