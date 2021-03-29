c0bd961cba593f359c0d4e20997f5ea099972d92
Vinícios Henrique <marcucci.vinicios@gmail.com>
2021-03-29 11:24:47 -0300

    editar atividade

--- src/pages/EditActivity/index.tsx
+++ src/pages/EditActivity/index.tsx
@@ -5,35 +5,44 @@ import './styles.css';
 export default function EditActivity() {
   return (
     <Template title="Professor">
+      <div className="esquerdaDireita">
+        <div className="tabelaEsquerda">
+          <h1 className="exemploAtividade">Atividades</h1>
 
-      <h1 id="nomeatividades">Atividades</h1>
-      <div className="todas">
-        <div className="avaliacoes">
-          <h1 id="avaliacao_nome">2° Informática - prova - 03/10</h1>
+          <div className="conteudoAvaliacoes">
 
+            <h1> 2° Informática - prova -  03/10 </h1>
+            <div className="conteudoAvaliacoes">
+              <h1> 2° Informática - prova -  03/10 </h1>
+            </div>
+          </div>
         </div>
-        <div className="avaliacoes">
-          <h1 id="avaliacao_nome">2° Informática - prova - 03/10</h1>
 
-        </div>
-        <div className="avaliacoes">
-          <h1 id="avaliacao_nome">2° Informática - prova - 03/10</h1>
+        <div className="tabelaDescricao">
 
-        </div>
-        <div className="avaliacoes">
-          <h1 id="avaliacao_nome">2° Informática - prova - 03/10</h1>
+          <div className="tabelaConteudo">
 
+            <h1 className="tituloTabela">PROVA DE REDES DE COMPUTADORES</h1>
+            <div className="subConteudo">
+              <p>
+                Escrever um resumo sobre as diferenças que os avaços na tecnologia
+                proveram ao mundo nos ultimos 70 anos.
+              </p>
+              <section className="notaDataTipo">
+                <span>Pontuação máxima: 8</span>
+                <span>Data de entrega: 03/10</span>
+                <span>tipo de atividade: Resumo</span>
+              </section>
+            </div>
+
+            <div className="botoesEditDelete">
+              <button className="editar" type="button">EDITAR</button>
+              <button className="excluir" type="button">EXCLUIR</button>
+
+            </div>
+          </div>
         </div>
       </div>
-      <div className="informacao">
-        <h1 id="titulo_informacao">2° Informática - prova -  03/10</h1>
-        <h1 id="descricao_informacao">Escrever um resumo sobre as diferenças que os avaços na tecnologia proveram ao mundo nos ultimos 70 anos.</h1>
-        <h1 id="pontuacao_informacao">Pontuação máxima: 8</h1>
-        <h1 id="data_informacao">Data de entrega: 03/10 </h1>
-        <h1 id="tipo_informacao">tipo de atividade: Resumo </h1>
-
-      </div>
-
     </Template>
   );
 }
