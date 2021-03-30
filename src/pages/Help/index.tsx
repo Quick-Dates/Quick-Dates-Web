291d5e2538c0866982498eacde6028f2c33e79b6
Willian17 <willianfernandes113@gmail.com>
2021-03-30 15:13:46 -0400

    refactor: tela help

--- src/pages/Help/index.tsx
+++ src/pages/Help/index.tsx
@@ -16,19 +16,21 @@ export default function Help() {
 
   return (
     <Template isStudent={user === 'student' ? true : false} title="Help">
-      <div className="socialMedia">
-        <a className="linksSocialMedia" href="https://web.telegram.org/#/login">
-          <img className="socialMediaIcon" src={TelegramIcon} alt="Telegram" />
-          <p className="nameSocialMedia">Telegram</p>
-        </a>
-        <a className="linksSocialMedia" href="https://www.gmail.com/mail/help/intl/en/about.html?utm_expid=...">
-          <img className="socialMediaIcon" src={GmailIcon} alt="Gmail" />
-          <p className="nameSocialMedia">Gmail</p>
-        </a>
-        <a className="linksSocialMedia" href="https://www.whatsapp.com/?lang=pt_br">
-          <img className="socialMediaIcon" src={WhatsAppIcon} alt="WhatsApp" />
-          <p className="nameSocialMedia"> WhatsApp </p>
-        </a>
+      <div className="containerSocialMedia">
+        <div className="socialMedia">
+          <a className="linksSocialMedia" href="https://t.me/WillianSalve">
+            <img className="socialMediaIcon" src={TelegramIcon} alt="Telegram" />
+            <p className="nameSocialMedia">Telegram</p>
+          </a>
+          <a className="linksSocialMedia" href="mailto:main.quickdates@gmail.com?subject=Olá%20QuickDates">
+            <img className="socialMediaIcon" src={GmailIcon} alt="Gmail" />
+            <p className="nameSocialMedia">Gmail</p>
+          </a>
+          <a className="linksSocialMedia" href="https://api.whatsapp.com/send?phone=556699730596&text=Ol%C3%A1%2C%20tenho%20uma%20d%C3%BAvida%20no%20QuickDates%20em%3A">
+            <img className="socialMediaIcon" src={WhatsAppIcon} alt="WhatsApp" />
+            <p className="nameSocialMedia"> WhatsApp </p>
+          </a>
+        </div>
       </div>
     </Template>
   );
