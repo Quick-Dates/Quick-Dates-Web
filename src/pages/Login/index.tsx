37e2770a1c39cd864e56b01e74b7c78c52486232
Vitor-Manoel1014 <vm53596@gmail.com>
2021-04-01 18:03:13 -0400

    EITA

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unused-vars *//**/
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './styles.css';
