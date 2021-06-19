0118ad152b2027d52bd16bbef8d9e2d469c26a80
Willian17 <willianfernandes113@gmail.com>
2021-06-18 23:59:08 -0400

    arrumando footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -7,17 +7,19 @@ import './styles.css';
 export default function Footer() {
   return (
     <footer className="footer">
-      <p className="text">&copy; Quick Dates 2020</p>
-      <p>main.quickdates@gmail.com</p>
-      <a href="https://www.linkedin.com/in/quick-dates-880850208" target="_blank">
-        <img src={LinkedinIcon} alt="linkedin" />
-      </a>
-      <a href="https://twitter.com/DatesQuick" target="_blank">
-        <img src={TwitterIcon} alt="twitter" />
-      </a>
-      <a href="https://www.instagram.com/quick.dates/" target="_blank">
-        <img src={InstagramIcon} alt="instagram" />
-      </a>
+      <p className="copyright">&copy; Quick Dates 2020</p>
+      <p className="email">main.quickdates@gmail.com</p>
+      <div className="container-social-media">
+        <a href="https://www.linkedin.com/in/quick-dates-880850208" target="_blank">
+          <img src={LinkedinIcon} alt="linkedin" />
+        </a>
+        <a href="https://twitter.com/DatesQuick" target="_blank">
+          <img src={TwitterIcon} alt="twitter" />
+        </a>
+        <a href="https://www.instagram.com/quick.dates/" target="_blank">
+          <img src={InstagramIcon} alt="instagram" />
+        </a>
+      </div>
     </footer>
   );
 }
