4143b42a46bc8d617896a74a02a90ad3e30bd5a4
Antonio Kennedy <antonioliveiraak@gmail.com>
2021-06-19 15:55:59 -0300

    header ranking

--- src/pages/Ranking/index.tsx
+++ src/pages/Ranking/index.tsx
@@ -1,18 +1,11 @@
 /* eslint-disable no-unneeded-ternary */
 import React from 'react';
-import { useParams } from 'react-router-dom';
 import Template from '../../components/Template';
 import './styles.css';
 
-interface IProps{
-  user:string
-}
-
 export default function Ranking() {
-  const { user }:IProps = useParams();
-
   return (
-    <Template titleTab="Ranking" isStudent={user === 'student' ? true : false} title="Ranking">
+    <Template titleTab="Ranking" isStudent title="Ranking">
       <div className="centrali">    <p className="textee">Ainda não sei como vou FAZER ESSE RANKING</p>  </div>
     </Template>
   );
