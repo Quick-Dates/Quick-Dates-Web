6540f30573c315a47bb8cd3fc2d1f2cfc72a7e3b
Gustavo Lários <gusttalarios14@gmail.com>
2021-06-22 18:19:45 -0400

    Pagina de tarefa

--- src/pages/ScheduleActivity/index.tsx
+++ src/pages/ScheduleActivity/index.tsx
@@ -1,8 +1,12 @@
-import { Form } from '@unform/web';
-import React from 'react';
-import Input from '../../components/Input';
-import Template from '../../components/Template';
-import './styles.css';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable quotes */
+import { Form } from "@unform/web";
+import React from "react";
+import Input from "../../components/Input";
+import Template from "../../components/Template";
+import "./styles.css";
+import Teacher from "../../assets/teacher.svg";
+import Today from "../../assets/schedule.svg";
 
 interface DataForm {
   title: string;
@@ -19,16 +23,149 @@ export default function ScheduleActivity() {
   }
   return (
     <Template title="Agendar Atividade" titleTab="Professor">
-      <div className="containerScheduleActivity">
+      <div className="schedule">
+        <div className="contentSchedule1">
+          <div className="contentSchedule1Left">
+            <p className="textSchedule">
+              Aqui você pode agendar atividades para seus alunos, assim ambos
+              tem um melhor controle sobre o tempo.
+            </p>
+
+            <h3 className="titleContentSchedule1">
+              Como marcar uma atividade?
+            </h3>
+
+            <p className="textSchedule">
+              Aqui você pode agendar atividades para seus alunos, assim ambos
+              tem um melhor controle sobre o tempo.
+            </p>
+          </div>
+          <img src={Teacher} alt="" />
+        </div>
+
+        <div className="contentSchedule2">
+          <div className="contentSchedule2Left">
+            <h3 className="titleContentSchedule2">
+              Preencha os campos ao lado
+            </h3>
+
+            <p className="textSchedule">
+              Aqui você fornecerá as informações necessárias para agendar as
+              atividades que desejar, desde que não exceda o limite de duas
+              atividades avaliativas por dia.
+            </p>
+            <img src={Today} alt="Today" className="Image" />
+          </div>
+          <div className="contentSchedule2Right">
+            <Form className="form-schedule" onSubmit={handleSubmit}>
+              <Input
+                type="text"
+                name="titulo"
+                className="textInput"
+                placeholder="Titulo"
+              />
+
+              <Input
+                type="date"
+                name="date"
+                className="date-input"
+                placeholder="Data"
+              />
+
+              <Input
+                type="time"
+                name="time-first"
+                className="timeInput"
+                placeholder="Hora de inicio"
+              />
+
+              <Input
+                type="time"
+                name="time-last"
+                className="timeInput"
+                placeholder="Hora de término"
+              />
+
+              <Input
+                list="bimestres"
+                type="text"
+                name="bimestre"
+                className="bimestreInput"
+                placeholder="Bimestre"
+              />
+
+              <datalist id="bimestres">
+                <option value="1° Bimestre">1° Bimestre</option>
+                <option value="2° Bimestre">2° Bimestre</option>
+                <option value="3° Bimestre">3° Bimestre</option>
+                <option value="4° Bimestre">4° Bimestre</option>
+              </datalist>
+
+              <Input
+                list="classes"
+                type="text"
+                name="class"
+                placeholder="Turma"
+              />
+
+              <datalist id="classes">
+                <option value="Informática">Informática</option>
+                <option value="Secretariado">Secretariado</option>
+                <option value="Alimentos">Alimentos</option>
+                <option value="Química">Química</option>
+              </datalist>
+              <Input
+                type="number"
+                min="1.0"
+                max="10.0"
+                step="0.1"
+                name="score"
+                placeholder="Pontuação"
+              />
+
+              <Input
+                list="tipoAtividade"
+                type="text"
+                name="type"
+                placeholder="Tipo de avaliação"
+              />
+
+              <datalist id="tipoAtividade">
+                <option value="Prova">Prova</option>
+                <option value="Lista de exercicio">Lista de exercicio</option>
+                <option value="Relatório">Relatório</option>
+                <option value="Seminário">Seminário</option>
+              </datalist>
+
+              <Input
+                type="text"
+                name="description"
+                className="descricao"
+                placeholder="Descrição"
+              />
+
+              <button className="buttonScheduleActivity" type="submit">
+                Agendar
+              </button>
+            </Form>
+          </div>
+        </div>
+      </div>
+
+      {/* <div className="containerScheduleActivity">
         <div className="content-shedule">
           <Form className="form-schedule" onSubmit={handleSubmit}>
             <h1 className="title-shedule">Agendar atividades</h1>
 
             <div className="containerAgendar">
-
               <Input type="text" name="title" placeholder="Título" />
 
-              <Input type="date" name="date" className="date-input" placeholder="Data" />
+              <Input
+                type="date"
+                name="date"
+                className="date-input"
+                placeholder="Data"
+              />
 
               <datalist id="classes">
                 <option value="Informática">Informática</option>
@@ -37,19 +174,30 @@ export default function ScheduleActivity() {
                 <option value="Química">Química</option>
               </datalist>
 
-              <Input list="classes" type="text" name="class" placeholder="Turma" />
+              <Input
+                list="classes"
+                type="text"
+                name="class"
+                placeholder="Turma"
+              />
 
               <Input type="number" name="score" placeholder="Pontuação" />
 
               <Input type="text" name="type" placeholder="Tipo de avaliação" />
 
-              <Input type="text" name="description" className="descricao" placeholder="Descrição" />
-
+              <Input
+                type="text"
+                name="description"
+                className="descricao"
+                placeholder="Descrição"
+              />
             </div>
-            <button className="buttonScheduleActivity" type="submit">Agendar</button>
+            <button className="buttonScheduleActivity" type="submit">
+              Agendar
+            </button>
           </Form>
         </div>
-      </div>
+      </div> */}
     </Template>
   );
 }
