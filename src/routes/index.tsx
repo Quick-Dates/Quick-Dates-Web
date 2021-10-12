f83a18d491c44a99600203475b4dca38446fc8c7
Willian17 <willianfernandes113@gmail.com>
2021-10-11 23:51:51 -0400

    delete tela horarios

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,6 @@ import { Switch } from 'react-router-dom';
 import ActivitiesList from '../pages/ActivitiesList';
 import Login from '../pages/Login';
 import NotFound404 from '../pages/NotFound404';
-import Time from '../pages/Time';
 import ScheduleActivity from '../pages/ScheduleActivity';
 import EditActivity from '../pages/EditActivity';
 import Ranking from '../pages/Ranking';
@@ -40,7 +39,6 @@ export default function Routes() {
     <Switch>
       <Route path="/signin" exact component={Login} />
       <Route path="/activities" component={ActivitiesList} isPrivate />
-      <Route path="/time" component={Time} isPrivate />
       <Route path="/grades" component={Grade} isPrivate />
       <Route path="/activity-details" component={ActitvityDetails} isPrivate />
       <Route path="/shedule-activity" component={ScheduleActivity} isPrivate />-
